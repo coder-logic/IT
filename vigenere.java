216e179f99438e2fd36a7b50fcc97ab41a497d18
coder-logic <d2024.prajwal.pawar@ves.ac.in>
2025-11-02 19:40:19 +0530

    Create vigenere.java

---
+++ vigenere.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+class VigenereCipher
+{
+
+// This function generates the key in
+// a cyclic manner until it's length isn't
+// equal to the length of original text
+static String generateKey(String str, String key)
+{
+    int x = str.length();
+
+    for (int i = 0; ; i++)
+    {
+        if (x == i)
+            i = 0;
+        if (key.length() == str.length())
+            break;
+        key+=(key.charAt(i));
+    }
+    return key;
+}
+
+// This function returns the encrypted text
+// generated with the help of the key
+static String cipherText(String str, String key)
+{
+    String cipher_text="";
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        // converting in range 0-25
+        int x = (str.charAt(i) + key.charAt(i)) %26;
+
+        // convert into alphabets(ASCII)
+        x += 'A';
+
+        cipher_text+=(char)(x);
+    }
+    return cipher_text;
+}
+
+// This function decrypts the encrypted text
+// and returns the original text
+static String originalText(String cipher_text, String key)
+{
+    key = generateKey(cipher_text,key);
+    String orig_text="";
+
+    for (int i = 0 ; i < cipher_text.length() && 
+                            i < key.length(); i++)
+    {
+        // converting in range 0-25
+        int x = (cipher_text.charAt(i) - 
+                    key.charAt(i) + 26) %26;
+
+        // convert into alphabets(ASCII)
+        x += 'A';
+        orig_text+=(char)(x);
+    }
+    return orig_text;
+}
+
+// This function will convert the lower case character to Upper case
+static String LowerToUpper(String s)
+{
+    StringBuffer str =new StringBuffer(s); 
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(Character.isLowerCase(s.charAt(i)))
+        {
+            str.setCharAt(i, Character.toUpperCase(s.charAt(i)));
+        }
+    }
+    s = str.toString();
+    return s;
+}
+
+// Driver code
+public static void main(String[] args) 
+{
+    
+    System.out.print("Enter a Plain text : ");
+    Scanner sc = new Scanner(System.in);
+    String Str = sc.nextLine();
+    System.out.print("Enter a Key : ");
+    String Keyword = sc.nextLine();
+    
+      String str = LowerToUpper(Str);
+      String keyword = LowerToUpper(Keyword);
+
+    String key = generateKey(str, keyword);
+    String cipher_text = cipherText(str, key);
+
+    System.out.println("Ciphertext : "
+        + cipher_text + "\n");
+    System.out.println("---------------------------------------------------");
+    System.out.print("Enter Cipher Text : ");
+    String ct = sc.nextLine();
+    System.out.print("Enter a Key : ");
+    String k = sc.nextLine();
+    System.out.println("Decrypted Text : "
+        + originalText(ct, k));
+    System.out.println("Executed By Abdul Qadeer");
+    }
+}
